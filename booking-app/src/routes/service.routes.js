705b54e10e519722f6cfe99c7649167d05d22f5b
phamxuanchung100860 <130673856+phamxuanchung100860@users.noreply.github.com>
2024-12-07 09:35:59 +0700

    Revert "up"
    This reverts commit d8cb93fab1c0405e685f5fb88ae3ac2d4f8a7701.

--- booking-app/src/routes/service.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getServices } = require('../controllers/service.controller');
-const router = express.Router();
-
-router.get('/', getServices);
-
-module.exports = router;
